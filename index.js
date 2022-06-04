db11ef63c66119ce2ae41b227eb2d7cdffab0052
caturhidayat <caturhidayat00@gmail.com>
2022-06-04 18:16:28 +0700

    add comment in index.js

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ import { engine } from "express-handlebars";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// hbs template engine
 app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views', './views');
 
+
+// ROUTE
 app.get("/", (req, res) => {
   res.render("home");
 });
