5d5ce1df81929bfb9e4bfa99ce1b1283818e431d
caturhidayat <caturhidayat00@gmail.com>
2022-06-04 18:07:46 +0700

    hbs setup with @types/express

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { engine } from "express-handlebars";
+
+const app = express();
+
+app.engine('.hbs', engine({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', './views');
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.listen(3000, () => {
+  console.log(`Server running at http://localhost:3000`);
+});
